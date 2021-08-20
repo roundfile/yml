30d2273ee5e708e7b7845d260d36863ad3db6e55
Dave <1648748+roundfile@users.noreply.github.com>
2021-08-20 14:00:27 -0700

    Update __init__.py

--- src/artisanlib/__init__.py
+++ src/artisanlib/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 __version__ = '2.4.7'
-__revision__ = '0'
-__build__ = '0'
+__revision__ = '5'
+__build__ = '8'
 
 __release_sponsor_name__ = "Sweet Maria's"
 __release_sponsor_domain__ = "sweetmarias.com"
