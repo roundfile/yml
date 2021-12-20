ed88eb6311acb6b58fc95e768f691ec96b014c33
Dave <1648748+roundfile@users.noreply.github.com>
2021-12-19 23:15:31 -0800

    build ui and help files

---
+++ src/test-build-win3-pi.bat
@@ -0,0 +1,45 @@
+@echo off
+
+:: comandline option LEGACY used to flag a legacy build in NSIS
+if "%~1" == "LEGACY"  (set ARTISAN_LEGACY="True") else (set ARTISAN_LEGACY="False")
+if "%~1" == "LEGACY"  (set ARTISAN_SPEC=win-legacy) else (set ARTISAN_SPEC=win)
+ 
+:: set the path to pyuic
+if "%APPVEYOR%" == "True" (
+    set PYTHON_PATH=%PYTHON%
+) else (
+    set PYTHON_PATH=c:\Python38-64
+)
+if %ARTISAN_LEGACY% == "True" (
+    set PYUIC=%PYTHON_PATH%\scripts\pyuic5.exe
+) else (
+    set PYUIC=%PYTHON_PATH%\scripts\pyuic6.exe
+)
+
+echo PYTHON_PATH %PYTHON_PATH%
+
+:: build the py files from ui files
+FOR /R %%a IN (ui\*.ui) DO (
+    echo %%~na
+    %PYUIC% -o uic\%%~na.py --from-imports ui\%%~na.ui
+)
+
+:: convert help files
+%PYTHON_PATH%\python.exe ..\doc\help_dialogs\Script\xlsx_to_artisan_help.py all
+
+rem rem set environment variables for version and build
+rem FOR /F "usebackq delims==" %%a IN (`python -c "import artisanlib; print(artisanlib.__version__)"`) DO (set ARTISAN_VERSION=%%~a)
+rem FOR /F "usebackq delims==" %%a IN (`python -c "import artisanlib; print(artisanlib.__build__)"`) DO (set ARTISAN_BUILD=%%~a)
+rem 
+rem rem create a version file for pyinstaller
+rem create-version-file version-metadata.yml --outfile version_info-win.txt --version %ARTISAN_VERSION%.%ARTISAN_BUILD%
+rem pyinstaller --noconfirm artisan-%ARTISAN_SPEC%.spec
+rem 
+rem rem Don't make assumptions as to where the 'makensis.exe' is - look in the obvious places
+rem if exist "C:\Program Files (x86)\NSIS\makensis.exe" set NSIS_EXE="C:\Program Files (x86)\NSIS\makensis.exe"
+rem if exist "C:\Program Files\NSIS\makensis.exe"       set NSIS_EXE="C:\Program Files\NSIS\makensis.exe"
+rem if exist "%ProgramFiles%\NSIS\makensis.exe"         set NSIS_EXE="%ProgramFiles%\NSIS\makensis.exe"
+rem if exist "%ProgramFiles(x86)%\NSIS\makensis.exe"    set NSIS_EXE="%ProgramFiles(x86)%\NSIS\makensis.exe"
+rem 
+rem rem build the installer
+rem %NSIS_EXE% /DPRODUCT_VERSION=%ARTISAN_VERSION%.%ARTISAN_BUILD% /DLEGACY=%ARTISAN_LEGACY% setup-install3-pi.nsi
