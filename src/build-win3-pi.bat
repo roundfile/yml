45e73c3c6b015e7b37a42a9a634a985797c8bc7c
Dave <1648748+roundfile@users.noreply.github.com>
2022-01-03 09:10:22 -0800

    start test translations

--- src/build-win3-pi.bat
+++ src/build-win3-pi.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :: comandline option LEGACY used to flag a legacy build in NSIS
-if "%~1" == "LEGACY"  (set ARTISAN_LEGACY=True) else (set ARTISAN_LEGACY=False)
+if "%~1" == "LEGACY"  (set ARTISAN_LEGACY="True") else (set ARTISAN_LEGACY="False")
 if "%~1" == "LEGACY"  (set ARTISAN_SPEC=win-legacy) else (set ARTISAN_SPEC=win)
  
 :: set the path to pyuic
@@ -12,34 +12,47 @@ if "%APPVEYOR%" == "True" (
 )
 if %ARTISAN_LEGACY% == "True" (
     set PYUIC=%PYTHON_PATH%\scripts\pyuic5.exe
+    set QT_PATH=%PYTHON_PATH%\lib\site-packages\qt5_applications\Qt
 ) else (
     set PYUIC=%PYTHON_PATH%\scripts\pyuic6.exe
+    set QT_PATH=%PYTHON_PATH%\lib\site-packages\qt6_applications\Qt
 )
 
-echo PYTHON_PATH %PYTHON_PATH%
+rem echo PYTHON_PATH %PYTHON_PATH%
 
-:: build the py files from ui files
-FOR /R %%a IN (ui\*.ui) DO (
-    echo %%~na
-    %PYUIC% -o uic\%%~na.py --from-imports ui\%%~na.ui
+:: Process trasnlation files
+echo *****Exectuting artisan.pro
+echo %QT_PATH%
+%QT_PATH%\bin\lrelease -verbose artisan.pro
+echo *****Done Executing artisan.pro
+FOR /R %%a IN (translations\qtbase_*.ts) DO (
+    echo *****Translation file %%~a
+    %QT_PATH%\bin\lrelease - verbose %%na
 )
+dir translations\qtbase_*.ts
 
-:: convert help files
-%PYTHON_PATH%\python.exe ..\doc\help_dialogs\Script\xlsx_to_artisan_help.py all
-
-rem set environment variables for version and build
-FOR /F "usebackq delims==" %%a IN (`python -c "import artisanlib; print(artisanlib.__version__)"`) DO (set ARTISAN_VERSION=%%~a)
-FOR /F "usebackq delims==" %%a IN (`python -c "import artisanlib; print(artisanlib.__build__)"`) DO (set ARTISAN_BUILD=%%~a)
-
-rem create a version file for pyinstaller
-create-version-file version-metadata.yml --outfile version_info-win.txt --version %ARTISAN_VERSION%.%ARTISAN_BUILD%
-pyinstaller --noconfirm artisan-%ARTISAN_SPEC%.spec
-
-rem Don't make assumptions as to where the 'makensis.exe' is - look in the obvious places
-if exist "C:\Program Files (x86)\NSIS\makensis.exe" set NSIS_EXE="C:\Program Files (x86)\NSIS\makensis.exe"
-if exist "C:\Program Files\NSIS\makensis.exe"       set NSIS_EXE="C:\Program Files\NSIS\makensis.exe"
-if exist "%ProgramFiles%\NSIS\makensis.exe"         set NSIS_EXE="%ProgramFiles%\NSIS\makensis.exe"
-if exist "%ProgramFiles(x86)%\NSIS\makensis.exe"    set NSIS_EXE="%ProgramFiles(x86)%\NSIS\makensis.exe"
-
-rem build the installer
-%NSIS_EXE% /DPRODUCT_VERSION=%ARTISAN_VERSION%.%ARTISAN_BUILD% /DLEGACY=%ARTISAN_LEGACY% setup-install3-pi.nsi
+rem :: build the py files from ui files
+rem FOR /R %%a IN (ui\*.ui) DO (
+rem     echo %%~na
+rem     %PYUIC% -o uic\%%~na.py --from-imports ui\%%~na.ui
+rem )
+rem 
+rem :: convert help files
+rem %PYTHON_PATH%\python.exe ..\doc\help_dialogs\Script\xlsx_to_artisan_help.py all
+rem 
+rem :: set environment variables for version and build
+rem FOR /F "usebackq delims==" %%a IN (`python -c "import artisanlib; print(artisanlib.__version__)"`) DO (set ARTISAN_VERSION=%%~a)
+rem FOR /F "usebackq delims==" %%a IN (`python -c "import artisanlib; print(artisanlib.__build__)"`) DO (set ARTISAN_BUILD=%%~a)
+rem 
+rem :: create a version file for pyinstaller
+rem create-version-file version-metadata.yml --outfile version_info-win.txt --version %ARTISAN_VERSION%.%ARTISAN_BUILD%
+rem pyinstaller --noconfirm artisan-%ARTISAN_SPEC%.spec
+rem 
+rem :: Don't make assumptions as to where the 'makensis.exe' is - look in the obvious places
+rem if exist "C:\Program Files (x86)\NSIS\makensis.exe" set NSIS_EXE="C:\Program Files (x86)\NSIS\makensis.exe"
+rem if exist "C:\Program Files\NSIS\makensis.exe"       set NSIS_EXE="C:\Program Files\NSIS\makensis.exe"
+rem if exist "%ProgramFiles%\NSIS\makensis.exe"         set NSIS_EXE="%ProgramFiles%\NSIS\makensis.exe"
+rem if exist "%ProgramFiles(x86)%\NSIS\makensis.exe"    set NSIS_EXE="%ProgramFiles(x86)%\NSIS\makensis.exe"
+rem 
+rem :: build the installer
+rem %NSIS_EXE% /DPRODUCT_VERSION=%ARTISAN_VERSION%.%ARTISAN_BUILD% /DLEGACY=%ARTISAN_LEGACY% setup-install3-pi.nsi
