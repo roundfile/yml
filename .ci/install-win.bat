004e1a81209f2b49c37b6b8e88ce65f190fe832e
Dave <1648748+roundfile@users.noreply.github.com>
2022-01-09 15:04:55 -0800

    tweek

--- .ci/install-win.bat
+++ .ci/install-win.bat
@@ -64,13 +64,13 @@ cd pyinstaller-%PYINSTALLER_VER%\bootloader
 cd ..
 %PYTHON_PATH%\\python.exe setup.py -q install
 cd ..
-# end: build the pyinstaller bootloader and install
+:: end: build the pyinstaller bootloader and install
 
 %PYTHON%\python.exe -m pip install -r src\\requirements.txt
 %PYTHON%\\python.exe -m pip install -r src\\requirements-win-legacy.txt
 curl -L -O %VC_REDIST%
-#curl -L -O https://aka.ms/vs/17/release/vc_redist.x64.exe
-# curl -L -O https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe
+::curl -L -O https://aka.ms/vs/17/release/vc_redist.x64.exe
+::curl -L -O https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe
 curl -k -L -O https://netcologne.dl.sourceforge.net/project/snap7/1.4.2/snap7-full-1.4.2.7z
 7z x snap7-full-1.4.2.7z
 copy snap7-full-1.4.2\build\bin\win64\snap7.dll c:\windows
