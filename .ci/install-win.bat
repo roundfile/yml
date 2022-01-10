b3fd881374dd2aab58aa07d8c729644c93260170
Dave <1648748+roundfile@users.noreply.github.com>
2022-01-09 17:10:24 -0800

    does this one work

--- .ci/install-win.bat
+++ .ci/install-win.bat
@@ -50,35 +50,35 @@ set PATH=%PYTHON_PATH%;%PYTHON_PATH%\Scripts;%PATH%
 
 echo Python Version
 python -V
-:: 
-:: %PYTHON_PATH%\python.exe -m pip install --upgrade pip
-:: %PYTHON_PATH%\python.exe -m pip install wheel
-:: 
-:: %PYTHON_PATH%\\python.exe -m pip install .ci\\pyinstaller-4.3-py3-none-any.whl
-:: :: build the pyinstaller bootloader and install
-rem curl -L -O https://github.com/pyinstaller/pyinstaller/archive/refs/tags/v%PYINSTALLER_VER%.zip
-rem 7z x v%PYINSTALLER_VER%.zip
-rem del v%PYINSTALLER_VER%.zip
-rem cd pyinstaller-%PYINSTALLER_VER%\bootloader
-rem %PYTHON_PATH%\python.exe ./waf all --target-arch=64bit
-rem cd ..
-rem %PYTHON_PATH%\python.exe setup.py -q install
-rem cd ..
+
+%PYTHON_PATH%\python.exe -m pip install --upgrade pip
+%PYTHON_PATH%\python.exe -m pip install wheel
+
+%PYTHON_PATH%\\python.exe -m pip install .ci\\pyinstaller-4.3-py3-none-any.whl
+:: build the pyinstaller bootloader and install
+curl -L -O https://github.com/pyinstaller/pyinstaller/archive/refs/tags/v%PYINSTALLER_VER%.zip
+7z x v%PYINSTALLER_VER%.zip
+del v%PYINSTALLER_VER%.zip
+cd pyinstaller-%PYINSTALLER_VER%\bootloader
+%PYTHON_PATH%\python.exe ./waf all --target-arch=64bit
+cd ..
+%PYTHON_PATH%\python.exe setup.py -q install
+cd ..
 :: end: build the pyinstaller bootloader and install
 
-:: %PYTHON%\python.exe -m pip install -r src\\requirements.txt
-:: %PYTHON%\python.exe -m pip install -r src\\requirements-win-legacy.txt
-:: echo curl vc_redist.x64.exe
-:: curl -L -O %VC_REDIST%
-:: ::curl -L -O https://aka.ms/vs/17/release/vc_redist.x64.exe
-:: ::curl -L -O https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe
-:: echo curl snap7
-:: curl -k -L -O https://netcologne.dl.sourceforge.net/project/snap7/1.4.2/snap7-full-1.4.2.7z
-:: 7z x snap7-full-1.4.2.7z
-:: copy snap7-full-1.4.2\build\bin\win64\snap7.dll c:\windows
-:: echo curl libusb-win32
-:: curl -k -L -O https://netcologne.dl.sourceforge.net/project/libusb-win32/libusb-win32-releases/1.2.6.0/libusb-win32-bin-1.2.6.0.zip
-:: 7z x libusb-win32-bin-1.2.6.0.zip
-:: copy libusb-win32-bin-1.2.6.0\bin\amd64\libusb0.dll C:\Windows\SysWOW64
-:: 
+%PYTHON%\python.exe -m pip install -r src\\requirements.txt
+%PYTHON%\python.exe -m pip install -r src\\requirements-win-legacy.txt
+echo curl vc_redist.x64.exe
+curl -L -O %VC_REDIST%
+::curl -L -O https://aka.ms/vs/17/release/vc_redist.x64.exe
+::curl -L -O https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe
+echo curl snap7
+curl -k -L -O https://netcologne.dl.sourceforge.net/project/snap7/1.4.2/snap7-full-1.4.2.7z
+7z x snap7-full-1.4.2.7z
+copy snap7-full-1.4.2\build\bin\win64\snap7.dll c:\windows
+echo curl libusb-win32
+curl -k -L -O https://netcologne.dl.sourceforge.net/project/libusb-win32/libusb-win32-releases/1.2.6.0/libusb-win32-bin-1.2.6.0.zip
+7z x libusb-win32-bin-1.2.6.0.zip
+copy libusb-win32-bin-1.2.6.0\bin\amd64\libusb0.dll C:\Windows\SysWOW64
+
  
