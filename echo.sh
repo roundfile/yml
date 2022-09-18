4ed65181c49c3dee074a122b12d2c2332e5729bd
Dave <1648748+roundfile@users.noreply.github.com>
2022-09-18 16:51:52 -0700

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -7,4 +7,5 @@ echo ${HOME}
 echo ${PYTHON}
 echo ${PYTHON_PATH}
 wget https://github.com/AppImage/pkg2appimage/releases
+ls releases
 #wget -c https://github.com/AppImage/pkg2appimage/releases/download/continuous/pkg2appimage-1807-x86_64.AppImage
