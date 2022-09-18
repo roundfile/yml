7b2ee5b35c29cd54d97f2662e8d7c556f528627f
Dave <1648748+roundfile@users.noreply.github.com>
2022-09-18 16:43:54 -0700

    fix test url

--- src/build-linux-pkg.sh
+++ src/build-linux-pkg.sh
@@ -77,8 +77,7 @@ mv *.deb ${NAME}.deb
 
 export ARCH=x86_64
 # Create AppImage by using the pkg2appimage tool
-#dave debug wget -c https://github.com/$(wget -q https://github.com/AppImage/pkg2appimage/releases -O - | grep "pkg2appimage-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2)
-wget -c https://github.com/AppImage/pkg2appimage/releases/pkg2appimage-1807-x86_64.AppImage
+wget -c https://github.com/$(wget -q https://github.com/AppImage/pkg2appimage/releases -O - | grep "pkg2appimage-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2)
 chmod +x ./pkg2appimage-*.AppImage
 ARCH=x86_64 ./pkg2appimage-*.AppImage artisan-AppImage.yml
 
