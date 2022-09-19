b466e9d3529270c248c0d455783fcb9be0cba1e1
Dave <1648748+roundfile@users.noreply.github.com>
2022-09-19 08:20:12 -0700

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -8,15 +8,15 @@
 #echo ${PYTHON_PATH}
 #wget -c https://github.com/AppImage/pkg2appimage/releases/download/continuous/pkg2appimage-1807-x86_64.AppImage
 echo ------------
-echo wget -c https://github.com/AppImage/pkg2appimage/releases
+echo wget -c https://github.com/AppImage/pkg2appimage/releases/expanded_assets/continuous
 echo ------------
-wget -c https://github.com/AppImage/pkg2appimage/releases -O foo.txt
+wget -c https://github.com/AppImage/pkg2appimage/releases/expanded_assets/continuous -O foo.txt
 echo ------------
 echo cat foo.txt
 echo ------------
 cat foo.txt
 
 echo ------------
-echo wget -c https://github.com/AppImage/pkg2appimage/releases -O - pipe grep "pkg2appimage-.*-x86_64.AppImage" pipe head -n 1 pipe cut -d '"' -f 2
+echo wget -c https://github.com/AppImage/pkg2appimage/releases/expanded_assets/continuous -O - pipe grep "pkg2appimage-.*-x86_64.AppImage" pipe head -n 1 pipe cut -d '"' -f 2
 echo ------------
-wget -c https://github.com/AppImage/pkg2appimage/releases -O - | grep "pkg2appimage-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2
+wget -c https://github.com/AppImage/pkg2appimage/releases/expanded_assets/continuous -O - | grep "pkg2appimage-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2
