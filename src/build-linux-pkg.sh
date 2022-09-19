0e4d539f164b0beeba78f4afe4c3adfa6278b9d4
Dave <1648748+roundfile@users.noreply.github.com>
2022-09-19 09:02:16 -0700

    Update build-linux-pkg.sh

--- src/build-linux-pkg.sh
+++ src/build-linux-pkg.sh
@@ -77,7 +77,7 @@ mv *.deb ${NAME}.deb
 
 export ARCH=x86_64
 # Create AppImage by using the pkg2appimage tool
-#dave debug wget -c https://github.com/$(wget -q https://github.com/AppImage/pkg2appimage/releases/expanded_assets/continuous -O - | grep "pkg2appimage-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2)
+wget -c https://github.com/$(wget -q https://github.com/AppImage/pkg2appimage/releases/expanded_assets/continuous -O - | grep "pkg2appimage-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2)
 ARCH=x86_64 ./pkg2appimage-*.AppImage artisan-AppImage.yml
 
 mv ./out/*.AppImage ${NAME}.AppImage
