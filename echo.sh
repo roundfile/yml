dc28abfabd46825758dcc7f344f2ced7fc024544
Dave <1648748+roundfile@users.noreply.github.com>
2022-09-19 08:03:03 -0700

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-echo ${HOME}
-#echo ${PASTA}
-#export FAZOOL=${HOME}/butterball
+#echo ${HOME}
+##echo ${PASTA}
+##export FAZOOL=${HOME}/butterball
 #echo ${FAZOOecho ${PYTON}echo ${PYTON}echo ${PYTON}L}
-echo ${PYTHON}
-echo ${PYTHON_PATH}
-#wget https://github.com/AppImage/pkg2appimage/releases
-#ls releases
+#echo ${PYTHON}
+#echo ${PYTHON_PATH}
 #wget -c https://github.com/AppImage/pkg2appimage/releases/download/continuous/pkg2appimage-1807-x86_64.AppImage
-echo wget -c https://github.com/AppImage/pkg2appimage/releases/pkg2appimage-1807-x86_64.AppImage
+echo ------------
+echo wget -c https://github.com/AppImage/pkg2appimage/releases
+echo ------------
 wget -c https://github.com/AppImage/pkg2appimage/releases -O foo.txt
+echo ------------
 echo cat foo.txt
+echo ------------
 cat foo.txt
+
+echo https://github.com/AppImage/pkg2appimage/releases -O - | grep "pkg2appimage-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2
+https://github.com/AppImage/pkg2appimage/releases -O - | grep "pkg2appimage-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2
