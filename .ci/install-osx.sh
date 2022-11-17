e273b86075c9b592229ce7c6c056d692389056dd
Dave <1648748+roundfile@users.noreply.github.com>
2022-11-17 09:33:39 -0800

    more

--- .ci/install-osx.sh
+++ .ci/install-osx.sh
@@ -54,6 +54,7 @@ sudo -H python3 -m pip install --root-user-action=ignore -r src/requirements-${A
 
 #debug
 sudo -H python3 -m pip freeze
+sudo -H python3 -m pip show python-snap7
 
 # copy the snap7 binary installed by pip
 #cp -f ${PYTHONPATH}/lib/site-packages/snap7/lib/libsnap7.dylib /usr/local/lib
