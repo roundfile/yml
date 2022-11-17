d63cd30da8476ad8d1093c45d4365655660ff04d
Dave <1648748+roundfile@users.noreply.github.com>
2022-11-17 02:08:02 -0800

    Update install-osx.sh

--- .ci/install-osx.sh
+++ .ci/install-osx.sh
@@ -52,7 +52,10 @@ python3 -m pip install --upgrade pip
 sudo -H python3 -m pip install --root-user-action=ignore -r src/requirements.txt
 sudo -H python3 -m pip install --root-user-action=ignore -r src/requirements-${ARTISAN_OS}.txt
 
+#debug
+sudo -H python3 pip show snap7
+
 # copy the snap7 binary installed by pip
-cp -f ${PYTHONPATH}/site-packages/snap7/lib/libsnap7.dylib /usr/local/lib
+cp -f ${PYTHONPATH}/lib/site-packages/snap7/lib/libsnap7.dylib /usr/local/lib
 
 
