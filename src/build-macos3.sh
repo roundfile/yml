b8026b80f53ac8f9adaae35648c2d7cabd6f7a03
Dave <1648748+roundfile@users.noreply.github.com>
2022-11-17 17:13:37 -0800

    typos

--- src/build-macos3.sh
+++ src/build-macos3.sh
@@ -83,4 +83,4 @@ fi
 rm -rf build dist
 sleep .3 # sometimes it takes a little for dist to get really empty
 echo "************* 3 **************"
-python3 setup-mac3.py py2app | egrep -v '^(creating|copying file|byte-compiling|locate)'
+python3 setup-macos3.py py2app | egrep -v '^(creating|copying file|byte-compiling|locate)'
