4a9e1ab60c61cd035f48611ed677829b8bdd3544
Dave <1648748+roundfile@users.noreply.github.com>
2023-02-05 18:05:15 -0800

    Update build-macos3.sh

--- src/build-macos3.sh
+++ src/build-macos3.sh
@@ -60,7 +60,7 @@ echo "************* 1 **************"
 
 if [ -f "$PYLUPDATE" ]; then
     ls -l /Users/appveyor/venv3.11.0/bin/pylupdate6
-    pylupdate6 -version
+    pylupdate6 -V
     which pylupdate6
     echo "*************"
     $PYLUPDATE artisan.pro
