8ce51f96f02a82efe7a3638b96a7b49d179ee00f
Dave <1648748+roundfile@users.noreply.github.com>
2023-05-06 07:50:28 -0700

    ready mac

--- src/build-macos3.sh
+++ src/build-macos3.sh
@@ -19,7 +19,7 @@ else
     export QT_PATH=${PYTHONPATH}/site-packages/PyQt6/Qt6
     export QT_SRC_PATH=~/Qt/6.4.1/macos
     export PYUIC=pyuic6
-    export PYRCC=pyrcc6
+    #remove export PYRCC=pyrcc6
     export PYLUPDATE=./pylupdate6pro
 
     export MACOSX_DEPLOYMENT_TARGET=10.15
@@ -35,7 +35,7 @@ echo "ls uic before PYUIC"
 ls ./uic
 
 # ui / qrc
-echo "************* 0 **************"
+echo "************* ui/uic **************"
 # ui
 find ui -iname "*.ui" | while read f
 do
@@ -47,7 +47,7 @@ do
         $PYUIC -o uic/${fn}.py -x ui/${fn}.ui
     fi
 done
-echo "ls uic before PYUIC"
+echo "ls uic after PYUIC"
 ls ./uic
 
 
@@ -56,8 +56,10 @@ echo "ls translations before pylupdate"
 ls translations
 
 if [ -f "$PYLUPDATE" ]; then
-echo "************* 1 **************"
+    echo "************* pylupdate **************"
     $PYLUPDATE artisan.pro
+else
+    echo "************* skip pylupdate **************"
 fi
 
 echo "ls translations after pylupdate"
@@ -73,7 +75,7 @@ ls translations
 ##ls ~/Qt/6.4/macos/bin/lrelease
 
 # there is no full Qt installation on Travis, thus don't run  lrelease
-echo "************* 2 **************"
+echo "************* lrelease **************"
 $QT_SRC_PATH/bin/lrelease -verbose artisan.pro
 for f in translations/qtbase_*.ts
 do
@@ -85,18 +87,18 @@ echo "ls translations after lrelease"
 ls translations
 
 echo "ls uic before Help"
-ls ./uic
+ls ./help
 
 # convert help files from .xlsx to .py
-echo "************* 3 **************"
+echo "************* help **************"
 python3 ../doc/help_dialogs/Script/xlsx_to_artisan_help.py all
 
 echo "ls uic after Help"
-ls ./uic
+ls ./help
 
 
 # distribution
 rm -rf build dist
 sleep .3 # sometimes it takes a little for dist to get really empty
-echo "************* 4 **************"
+echo "************* p2app **************"
 python3 setup-macos3.py py2app | egrep -v '^(creating|copying file|byte-compiling|locate)'
