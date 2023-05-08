d34db9c1a038c66292c5daefd6847ba468d8bdcd
Dave <1648748+roundfile@users.noreply.github.com>
2023-05-07 23:23:18 -0700

    Update build-linux.sh

--- src/build-linux.sh
+++ src/build-linux.sh
@@ -71,8 +71,8 @@ echo "--------"
 #echo "ls -l /home/appveyor/venv${PYTHON_V}/lib/python${PYTHON_V}/site-packages/PyQt6/Qt6"
 #ls -l /home/appveyor/venv${PYTHON_V}/lib/python${PYTHON_V}/site-packages/PyQt6/Qt6
 
-echo "BEFORE ls -l translations/artisan_ar.ts"
-ls - l translations/artisan_ar.ts
+#echo "BEFORE ls -l translations/artisan_ar.ts"
+#ls - l translations/artisan_ar.ts
 
 if [ -f "$PYLUPDATE" ]; then
     echo "************* pylupdate **************"
