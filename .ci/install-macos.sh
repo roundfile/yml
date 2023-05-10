0b0b29c02cc4d1b274a54b8eb2c87a58ca094c29
Dave <1648748+roundfile@users.noreply.github.com>
2023-05-10 12:56:23 -0700

    code in script

--- .ci/install-macos.sh
+++ .ci/install-macos.sh
@@ -18,46 +18,26 @@
 #set -ex # increased logging
 set -e # reduced logging
 
+echo "** Running install-macos.sh"
+
 #.ci/silence.sh brew update # this seems to help to work around some homebrew issues; and fails on others
 
-#------------
-# Python 3.7.5 is installed by default
-# to update use either:
-#brew upgrade python
-# or, to avoid issues with brew auto updates by deactivating them,
-#HOMEBREW_NO_AUTO_UPDATE=1 brew install python
-
-#brew uninstall numpy gdal postgis
-#brew unlink python
-#brew upgrade python
-
-#brew install python@3.8
-#brew link --force --overwrite python@3.8
-
-
-# following https://stackoverflow.com/questions/51125013/how-can-i-install-a-previous-version-of-python-3-in-macos-using-homebrew/51125014#51125014
-# to install Python 3.6.5
-#brew remove --ignore-dependencies python 1>/dev/null 2>&1
-#brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/f2a764ef944b1080be64bd88dca9a1d80130c558/Formula/python.rb 1>/dev/null 2>&1
-#------------
-
-## upgrade python from 3.9 to 3.10
-# 3.10.2 now already installed on AppVeyor
-#brew install python@3.10
-#brew unlink python@3.9
-#brew link --force python@3.10
-#export PATH="/usr/local/opt/python@$3.10/bin:$PATH"
-
-##brew update && brew upgrade python
-##brew unlink python@3.11 && brew link --force python@3.11
-###export PATH="/usr/local/Cellar/python@3.11/3.11.3bin:$PATH"
-### upgrade venv
-##python3 -m venv --upgrade /Users/appveyor/venv3.11.0
-### or create new
-###python3.11 -m venv /Users/appveyor/venv3.11.3
-##echo $PATH
-
-echo "** Running install-macos.sh"
+## upgrade Python
+# first deactivate current venv
+source $VIRTUAL_ENV/bin/activate
+deactivate
+# brew update Python
+brew update && brew upgrade python
+# relink Python
+brew unlink python@$PYTHON_V && brew link --force python@$PYTHON_V
+# add path
+export PATH="$(brew --prefix)/Cellar/python@$PYTHON_V/$PYUPGRADE_V/bin:$PATH"
+# create new venv
+python3 -m venv /Users/appveyor/venv$PYUPGRADE_V
+source /Users/appveyor/venv$PYUPGRADE_V/bin/activate
+# update symbolic link venv3.11 to point to our new venv3.11.3
+ln -vfns /Users/appveyor/venv$PYUPGRADE_V /Users/appveyor/venv$PYTHON_V
+export PATH=/Users/appveyor/venv$PYUPGRADE_V/bin:$PATH # not exported?
 
 hash -r
 uname -srv
