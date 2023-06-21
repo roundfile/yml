49ab4b8c286daf5aeda299f3ce2ed9701b501cf3
Dave <1648748+roundfile@users.noreply.github.com>
2023-06-21 15:31:03 -0700

    Update build-derived-win.bat

--- src/build-derived-win.bat
+++ src/build-derived-win.bat
@@ -72,8 +72,8 @@ echo ** Success
  
 :: Process translation files
 rem call "%VCVARSALL%" x86_amd64
-dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\"
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 echo ************* pylupdate **************
 if /i "%ARTISAN_LEGACY%" == "True" (
     echo *** Processing translation files defined in artisan.pro with pylupdate5.py
