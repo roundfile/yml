0238bd6087a841719e6b63166dbf28459ba0d186
Dave <1648748+roundfile@users.noreply.github.com>
2023-06-24 16:31:42 -0700

    dirs

--- src/build-win3-pi.bat
+++ src/build-win3-pi.bat
@@ -69,8 +69,12 @@ for /f "usebackq delims==" %%a IN (`python -c "import artisanlib; print(artisanl
 :: create a version file for pyinstaller
 create-version-file version-metadata.yml --outfile version_info-win.txt --version %ARTISAN_VERSION%.%ARTISAN_BUILD%
 
-::echo Running dir to find qtbase_de.qm
-::dir "\qtbase_de.qm" /S
+echo Running dir to find phidget22.dll
+dir "\phidget22.dll" /S
+dir "\snap7.dll" /S
+dir "\yapi.dll" /S
+dir "\yapi64.dll" /S
+dir "\libusb0.dll" /S
 ::
 :: run pyinstaller
 :: Choose log-level from 'TRACE', 'DEBUG', 'INFO', 'WARN', 'DEPRECATION', 'ERROR', 'FATAL'
