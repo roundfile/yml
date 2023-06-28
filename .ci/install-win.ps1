c1ca3993f1b6a53c65126c678dbedb3c65ed42f5
Dave <1648748+roundfile@users.noreply.github.com>
2023-06-27 17:33:41 -0700

    Update install-win.ps1

--- .ci/install-win.ps1
+++ .ci/install-win.ps1
@@ -123,8 +123,9 @@ $extractedFolder = "libusb-win32-bin-$env:LIBUSB_VER"
 $targetPath = "C:\Windows\SysWOW64\libusb0.dll"
 Invoke-WebRequest -Uri $downloadUrl -OutFile $zipFilePath -UseBasicParsing
 if (-not (Test-Path $zipFilePath)) {exit 106}
+if ((Test-Path $zipFilePath)) {Write-Host "Exists $zipFilePath"}
 Write-Host "Before unzip $zipFilePath"
-7z x ./$zipFilePath
+7z x "$zipFilePath"
 Write-Host "After unzip"
 Copy-Item ".\$extractedFolder\bin\amd64\libusb0.dll" $targetPath -Force
 if (-not (Test-Path $targetPath)) {exit 107}
