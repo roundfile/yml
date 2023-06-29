e50da4cf30978583b3174280af8dede8e5b2f472
Dave <1648748+roundfile@users.noreply.github.com>
2023-06-29 10:07:21 -0700

    typo

--- .ci/install-win.bat
+++ .ci/install-win.bat
@@ -22,7 +22,7 @@ setlocal enabledelayedexpansion
 if /i "%APPVEYOR%" NEQ "True" (
     echo This file is for use on Appveyor CI only.
     exit /b 1
-}
+)
 if /i "%ARTISAN_LEGACY%" NEQ "True" (
     set ARTISAN_SPEC=win
 ) else (
