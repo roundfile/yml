5f148559ded474f7f17d749f9ec95f6f43dd5667
Dave <1648748+roundfile@users.noreply.github.com>
2023-07-10 08:08:34 -0700

    remove with suppress

--- src/artisanlib/main.py
+++ src/artisanlib/main.py
@@ -25369,10 +25369,10 @@ def main() -> None:
 #        except Exception: # pylint: disable=broad-except
 #            pass
 
-    if not sys.platform.startswith('darwin') and appFrozen():
-        # bottle.py used by WebLCDs requires stdout and stderr to exist, which is not the case on Windows/Linux with PyInstaller >= 5.8.0
-        sys.stderr = _log.error
-        sys.stdout = _log.info
+#dave    if not sys.platform.startswith('darwin') and appFrozen():
+#        # bottle.py used by WebLCDs requires stdout and stderr to exist, which is not the case on Windows/Linux with PyInstaller >= 5.8.0
+#        sys.stderr = _log.error
+#        sys.stdout = _log.info
             
     QTimer.singleShot(700, appWindow.qmc.startPhidgetManager)
 
@@ -25383,8 +25383,9 @@ def main() -> None:
         logging.captureWarnings(True)
         with warnings.catch_warnings():
             warnings.simplefilter('ignore')
-            with suppress_stdout_stderr():
-                app.exec()
+#dave            with suppress_stdout_stderr():
+#dave             app.exec()
+         app.exec()  #dave
         # alternative:
         # ret = app.exec()
         # app = None
