431fea399da7b87f7783aa0dbd33070403087b40
Dave <1648748+roundfile@users.noreply.github.com>
2023-07-25 16:53:06 -0700

    Update __init__.py

--- src/artisanlib/__init__.py
+++ src/artisanlib/__init__.py
@@ -1,6 +1,6 @@
-__version__ = '2.8.4'
-__revision__ = '0'
-__build__ = '0'
+__version__ = '2.8.5'
+__revision__ = '1'
+__build__ = '1'
 
 __release_sponsor_name__ = 'BC Roasters'
 __release_sponsor_domain__ = 'bcroasters.com'
