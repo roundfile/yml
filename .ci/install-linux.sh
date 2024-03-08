e87fde3d5369c04f31f45dcf0ad95a9019d7a87a
roundfile <1648748+roundfile@users.noreply.github.com>
2024-03-08 13:58:15 -0800

    force use QTTOOLS

--- .ci/install-linux.sh
+++ .ci/install-linux.sh
@@ -22,8 +22,8 @@ which python3
 python3 --version
 
 #dave
-ls 
-ls Qt
+ls /home/appveyor 
+ls /hme/appveyor/Qt
 
 sudo apt-get update -y -q --allow-releaseinfo-change
 sudo apt-get install -y -q ruby-dev build-essential p7zip-full rpm gdb libudev-dev libfuse2
