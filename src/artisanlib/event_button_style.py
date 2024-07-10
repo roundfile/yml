21fb75c5454ae4bd8ad47d2defd23080c6d42b4a
roundfile <1648748+roundfile@users.noreply.github.com>
2024-07-10 12:02:22 -0700

    upto d153f2 plus build files

--- src/artisanlib/event_button_style.py
+++ src/artisanlib/event_button_style.py
@@ -2,43 +2,17 @@
 
 from artisanlib.util import createGradient
 
-artisan_event_button_style: str = """
-            EventPushButton {{ 
-                min-width: {min_width}px;
-                min-height: {min_height}px;
-                font-size: {default_font_size}pt;
+artisan_event_button_style = """
+            QPushButton {{ 
+                min-width: {min_width};
+                font-size: {default_font_size};
                 font-weight: bold;
-                padding: {padding}px;
+                padding: 3px;
                 border-style:solid;
                 border-radius:4;
                 border-color:grey;
                 border-width:0;
                 color: white;
-            }}
-            
-            EventPushButton[Selected=true] {{
-                font-size: {selected_font_size}pt;
-                background-color:""" + createGradient('#d4336a') + """ ;
-            }}
-            EventPushButton[Selected=true]:flat {{
-                color: darkgrey;
-                background-color: #f9e2ea;
-            }}
-            EventPushButton[Selected=true]:flat:!pressed:hover {{
-                color: #F5F5F5;
-                background-color: #e687a8;
-            }}
-            EventPushButton[Selected=true]:flat:pressed {{
-                color: #EEEEEE;
-                background-color: #d4336a;
-            }}
-            EventPushButton[Selected=true]:!flat:pressed {{
-                color: white;
-                background-color:""" + createGradient('#A61145') + """ ;
-            }}
-            EventPushButton[Selected=true]:!pressed:hover {{
-                color: white;
-                background-color:""" + createGradient('#cc0f50') + """ ;
             }}
             
             MajorEventPushButton[Selected=false]:flat {{
@@ -61,6 +35,7 @@ artisan_event_button_style: str = """
                 background-color:""" + createGradient('#1985ba') + """ ;
             }}
             
+            
             MinorEventPushButton[Selected=false]:flat {{
                 color: #BDBDBD;
                 background-color: #EEEEEE;
@@ -80,6 +55,33 @@ artisan_event_button_style: str = """
             MinorEventPushButton[Selected=false]:!pressed:hover {{
                 background-color:""" + createGradient('#43a7cf') + """ ;
             }}
+            
+
+            QPushButton[Selected=true] {{
+                font-size: {selected_font_size};
+                background-color:""" + createGradient('#d4336a') + """ ;
+            }}
+            QPushButton[Selected=true]:flat {{
+                color: darkgrey;
+                background-color: #f9e2ea;
+            }}
+            QPushButton[Selected=true]:flat:!pressed:hover {{
+                color: #F5F5F5;
+                background-color: #e687a8;
+            }}
+            QPushButton[Selected=true]:flat:pressed {{
+                color: #EEEEEE;
+                background-color: #d4336a;
+            }}
+            QPushButton[Selected=true]:!flat:pressed {{
+                color: white;
+                background-color:""" + createGradient('#A61145') + """ ;
+            }}
+            QPushButton[Selected=true]:!pressed:hover {{
+                color: white;
+                background-color:""" + createGradient('#cc0f50') + """ ;
+            }}
+            
             
             AuxEventPushButton[Selected=false]:pressed {{
                 background-color:""" + createGradient('#757575') + """ ;
