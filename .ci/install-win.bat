8dd4b739fca5258e0d752da648c7da10ebb7a5fc
roundfile <1648748+roundfile@users.noreply.github.com>
2024-07-22 23:37:02 -0700

    is it really the pipe and pyinstaller version

--- .ci/install-win.bat
+++ .ci/install-win.bat
@@ -58,20 +58,20 @@ if NOT "%PYUPGRADE_WIN_V%" == "" (
 ::
 :: get pip up to date
 ::
-python -m pip install --upgrade pip # pip update to 24.1 breaks CI
+:: python -m pip install --upgrade pip # pip update to 24.1 breaks CI
 python -m pip install wheel
 
 ::
 :: install Artisan required libraries from pip
 ::
-::python -m pip install -r src\requirements.txt | findstr /v /b "Ignoring"
+python -m pip install -r src\requirements.txt | findstr /v /b "Ignoring"
 :: SPEW EVERYTHING
-python -m pip install -r src\requirements.txt 
+::python -m pip install -r src\requirements.txt 
 
 :: #dave  added install build
-echo.
-echo Explicit (re)install of build v1.2.1
-python -m pip install build
+::echo.
+::echo Explicit (re)install of build v1.2.1
+::python -m pip install build
 
 ::
 :: custom build the pyinstaller bootloader or install a prebuilt
