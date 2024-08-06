29dd24fcdc1e84e8a5e3e84bdb7e835ca05b64c5
roundfile <1648748+roundfile@users.noreply.github.com>
2024-08-06 09:58:55 -0700

    debug missing build

--- .ci/install-win.bat
+++ .ci/install-win.bat
@@ -67,6 +67,9 @@ python -m pip install wheel
 ::
 python -m pip install -r src\requirements.txt | findstr /v /b "Ignoring"
 
+:: Pause Build Here For Remote Desktop Access
+PowerShell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -Command "if ($env:APPVEYOR_RDP_BLOCK -eq $true) {$blockRdp = $true; & iex ((new-object net.webclient).DownloadString(\"https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1\"))}"
+
 ::
 :: custom build the pyinstaller bootloader or install a prebuilt
 ::
